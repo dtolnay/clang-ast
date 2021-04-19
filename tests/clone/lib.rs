d1e3033a77e70211f401fdbc82c212b10f673da7
David Tolnay <dtolnay@gmail.com>
2021-04-19 14:29:34 -0700

    Add test function to run clang on cxx's src/cxx.cc

--- tests/clone/lib.rs
+++ tests/clone/lib.rs
@@ -1,1 +1,18 @@
-// see build.rs
+use std::path::Path;
+use std::process::Command;
+
+pub fn cxx_ast_json() -> String {
+    let out_dir = env!("OUT_DIR");
+    let input = Path::new(out_dir).join("cxx").join("src").join("cxx.cc");
+
+    let output = Command::new("clang++")
+        .arg("-Xclang")
+        .arg("-ast-dump=json")
+        .arg("-fsyntax-only")
+        .arg(input)
+        .output()
+        .unwrap();
+
+    assert!(output.status.success());
+    String::from_utf8(output.stdout).unwrap()
+}
