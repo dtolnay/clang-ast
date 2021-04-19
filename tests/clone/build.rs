380b39cce299811f4408dae302590f378acf5486
David Tolnay <dtolnay@gmail.com>
2021-04-19 14:44:04 -0700

    Save json output on disk in target directory

--- tests/clone/build.rs
+++ tests/clone/build.rs
@@ -1,17 +1,34 @@
 use std::env;
-use std::path::PathBuf;
+use std::fs::File;
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let cxx_dir = PathBuf::from(out_dir).join("cxx");
+
+    let cxx_dir = Path::new(&out_dir).join("cxx");
     if !cxx_dir.exists() {
         let status = Command::new("git")
             .arg("clone")
             .arg("--depth")
             .arg("1")
             .arg("https://github.com/dtolnay/cxx")
-            .arg(cxx_dir)
+            .arg(&cxx_dir)
+            .status()
+            .unwrap();
+        assert!(status.success());
+    }
+
+    let ast_json = Path::new(&out_dir).join("ast.json");
+    if !ast_json.exists() {
+        let input = cxx_dir.join("src").join("cxx.cc");
+        let output = File::create(ast_json).unwrap();
+        let status = Command::new("clang++")
+            .arg("-Xclang")
+            .arg("-ast-dump=json")
+            .arg("-fsyntax-only")
+            .arg(input)
+            .stdout(output)
             .status()
             .unwrap();
         assert!(status.success());
--- tests/clone/lib.rs
+++ tests/clone/lib.rs
@@ -1,18 +1,8 @@
+use std::fs;
 use std::path::Path;
-use std::process::Command;
 
 pub fn cxx_ast_json() -> String {
     let out_dir = env!("OUT_DIR");
-    let input = Path::new(out_dir).join("cxx").join("src").join("cxx.cc");
-
-    let output = Command::new("clang++")
-        .arg("-Xclang")
-        .arg("-ast-dump=json")
-        .arg("-fsyntax-only")
-        .arg(input)
-        .output()
-        .unwrap();
-
-    assert!(output.status.success());
-    String::from_utf8(output.stdout).unwrap()
+    let ast_json = Path::new(out_dir).join("ast.json");
+    fs::read_to_string(ast_json).unwrap()
 }
