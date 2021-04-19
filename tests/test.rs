2cc0699056c530990e67b8150b8557b7f8ae7751
David Tolnay <dtolnay@gmail.com>
2021-04-19 14:33:19 -0700

    Test deserialization into an enum

---
+++ tests/test.rs
@@ -0,0 +1,34 @@
+use serde::Deserialize;
+
+pub type Node = clang_ast::Node<Clang>;
+
+#[derive(Deserialize)]
+pub enum Clang {
+    NamespaceDecl(NamespaceDecl),
+    EnumDecl(EnumDecl),
+    EnumConstantDecl(EnumConstantDecl),
+    Unknown,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct NamespaceDecl {
+    pub name: Option<String>,
+    #[serde(rename = "isInline", default)]
+    pub is_inline: bool,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct EnumDecl {
+    pub name: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct EnumConstantDecl {
+    pub name: String,
+}
+
+#[test]
+fn test() {
+    let json = clang_ast_test_suite::cxx_ast_json();
+    let _: Node = serde_json::from_str(&json).unwrap();
+}
