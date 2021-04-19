d31904e9a1b5c2e1aba658dcb3631c626586677d
David Tolnay <dtolnay@gmail.com>
2021-04-19 13:23:52 -0700

    Limit clone to latest revision only

--- tests/clone/build.rs
+++ tests/clone/build.rs
@@ -6,6 +6,8 @@ fn main() {
     if !out.exists() {
         let status = Command::new("git")
             .arg("clone")
+            .arg("--depth")
+            .arg("1")
             .arg("https://github.com/dtolnay/cxx")
             .arg(out)
             .status()
