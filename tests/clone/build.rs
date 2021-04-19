847e09c6c325859abfbaf76c37eeb293be2c5329
David Tolnay <dtolnay@gmail.com>
2021-04-19 13:27:16 -0700

    Move cxx clone to clang-ast-test-suite's OUT_DIR

--- tests/clone/build.rs
+++ tests/clone/build.rs
@@ -1,15 +1,17 @@
+use std::env;
+use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    let scratch = scratch::path("clang-ast-test-suite");
-    let out = scratch.join("cxx");
-    if !out.exists() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let cxx_dir = PathBuf::from(out_dir).join("cxx");
+    if !cxx_dir.exists() {
         let status = Command::new("git")
             .arg("clone")
             .arg("--depth")
             .arg("1")
             .arg("https://github.com/dtolnay/cxx")
-            .arg(out)
+            .arg(cxx_dir)
             .status()
             .unwrap();
         assert!(status.success());
