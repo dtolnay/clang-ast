fd27c13733d6c41837c5cf9d30427488f04092b7
David Tolnay <dtolnay@gmail.com>
2022-10-07 22:31:29 -0700

    Add clang++-14

--- tests/clone/build.rs
+++ tests/clone/build.rs
@@ -11,7 +11,13 @@ use std::path::Path;
 use std::process::Command;
 
 // Executable names to try.
-static CLANG: &[&str] = &["clang++-13", "clang++-12", "clang++-11", "clang++"];
+static CLANG: &[&str] = &[
+    "clang++-14",
+    "clang++-13",
+    "clang++-12",
+    "clang++-11",
+    "clang++",
+];
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
