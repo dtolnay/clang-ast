c60243936b394d6d370e100ff4b301399e60567e
David Tolnay <dtolnay@gmail.com>
2023-01-04 22:29:04 -0800

    Release 0.1.12

--- src/lib.rs
+++ src/lib.rs
@@ -386,7 +386,7 @@
 //! and comparible more cheaply than a string. You may find yourself with lots
 //! of hashtables keyed on `Id`.
 
-#![doc(html_root_url = "https://docs.rs/clang-ast/0.1.11")]
+#![doc(html_root_url = "https://docs.rs/clang-ast/0.1.12")]
 #![allow(
     clippy::blocks_in_if_conditions,
     clippy::match_like_matches_macro,
