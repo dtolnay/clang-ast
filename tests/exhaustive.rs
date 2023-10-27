569fd2800e796816e780a8db4c3105a80a21d4fa
David Tolnay <dtolnay@gmail.com>
2023-10-26 19:48:16 -0700

    Add SubstTemplateTypeParmPackType field containsUnexpandedPack

--- tests/exhaustive.rs
+++ tests/exhaustive.rs
@@ -2539,6 +2539,8 @@ pub struct SubstTemplateTypeParmPackType {
     pub is_dependent: bool,
     #[serde(rename = "isInstantiationDependent", default)]
     pub is_instantiation_dependent: bool,
+    #[serde(rename = "containsUnexpandedPack", default)]
+    pub contains_unexpanded_pack: bool,
 }
 
 #[derive(Deserialize, Debug)]
